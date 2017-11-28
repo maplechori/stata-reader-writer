dd8d102c299723155f77a56c5e3d3416f175deef
Adrian Montero <maplechori@gmail.com>
2017-11-28 08:19:29 -0800

    time to begin the exports

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ unsigned short ops;
 
 // Options
 //
-// Read file - check metadata
+// Read file - check metadata - done
 // Read file - export to database
 // Read database - export to file
 
@@ -49,23 +49,27 @@ int main(int argc, char **argv)
     po::options_description desc("General options");
     desc.add_options()
     ("help", "produce help message")
-    ("stata-file", po::value< string >(), "stata file")
+    ("stata-file", po::value< string >(), "stata input file")
     ;
 
-    po::options_description db_export("Export options");
+    po::options_description db_export("Database options");
     db_export.add_options()
-    ("sqlite", po::value< string >(), "sqlite database filename")
+    ("db-type,d", po::value< string >(), "select database type [sqlite]")
+    ("connection-string,c", po::value< string >(), "connection string" )
     ;
 
     po::positional_options_description p;
     p.add("stata-file", -1);
+
+    po::positional_options_description db;
+    db.add("db-type", 1).add("connection-string", 2);
     
     po::options_description all("Allowed options");
     all.add(desc).add(db_export);
     
     po::variables_map args;
     
-    po::store (po::command_line_parser(argc, argv).options(all).positional(p)
+    po::store (po::command_line_parser(argc, argv).options(all).positional(p).positional(db)
     .style (po::command_line_style::default_style | po::command_line_style::allow_long_disguise)
     .run (), args);
 
@@ -74,7 +78,7 @@ int main(int argc, char **argv)
     if (args.count("help") || !args.count("stata-file"))
     {
       cout << "Usage: " << PROGRAM_NAME << " [options] <stata file>\n";
-      cout << all;
+      cout << all << endl;
       return 0;
     }
 
@@ -88,16 +92,39 @@ int main(int argc, char **argv)
 
     
  
-    if (args.count("db") && args.count("filename"))
+    if (args.count("db-type"))
     {
-      cout << "Exporting to " << args["db"].as< string >().c_str() << endl;
-      //ctx->exportToDB((char *)args["db"].as< string >().c_str());
+      DatabaseTypes db_type = SQLITE;
+
+      if (!args.count("connection-string"))
+      {
+        cout << "Connection string required" << endl;
+        return 0;     
+      }
+     
+      if (args["db-type"].as< string >() == "sqlite")
+      {
+        db_type = SQLITE;
+      }
+      else
+      {
+        cout << "Database type not supported" << endl;
+        return 0;
+      }
+    
+      cout << "Exporting to " << args["connection-string"].as< string >().c_str() << endl;
+      ctx->exportToDB( db_type, (char *)args["connection-string"].as< string >().c_str()); 
+      return 1;
+       
+        
+      
     }
     else
     {
         // Show the metadata of the file and exit 
          cout << "Information on Stata file: " << args["stata-file"].as< string >() << endl;
          cout << ctx->hdr.showHeader() << endl;
+         return 1;
     }
 
   }
